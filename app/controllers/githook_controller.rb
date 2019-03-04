8380d303c9deed809facf65eb810ce989c705c15
山田 俊輔 <yshunske@gmail.com>
2019-03-04 17:20:16 +0900

    fix: unnecessary code

--- app/controllers/githook_controller.rb
+++ app/controllers/githook_controller.rb
@@ -1,7 +1,5 @@
 class GithookController < ApplicationController
   def change_label
-    logger.info(params)
-    # render json: { result: 'ok' }.to_json
     render json: params.to_json
   end
 end
