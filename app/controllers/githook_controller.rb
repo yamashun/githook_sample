ff67c6cd081eb4411c74a759af4be2d3ebea7823
山田 俊輔 <yshunske@gmail.com>
2019-03-04 17:02:15 +0900

    change: change http method

--- app/controllers/githook_controller.rb
+++ app/controllers/githook_controller.rb
@@ -1,6 +1,7 @@
 class GithookController < ApplicationController
   def change_label
     logger.info(params)
-    render json: { result: 'ok' }.to_json
+    # render json: { result: 'ok' }.to_json
+    render json: params.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/githook/change_label', to: 'githook#change_label'
+  post '/githook/change_label', to: 'githook#change_label'
 end
